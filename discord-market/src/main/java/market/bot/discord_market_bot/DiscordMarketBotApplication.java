f9445be90c79b597266632c219b0025958d62c04
Zwukii <alen.smlatic2002@gmail.com>
2026-06-23 18:06:14 +0200

    add folder to github

---
+++ discord-market/src/main/java/market/bot/discord_market_bot/DiscordMarketBotApplication.java
@@ -0,0 +1,13 @@
+package market.bot.discord_market_bot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiscordMarketBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiscordMarketBotApplication.class, args);
+	}
+
+}
---
+++ discord-market/src/test/java/market/bot/discord_market_bot/DiscordMarketBotApplicationTests.java
@@ -0,0 +1,13 @@
+package market.bot.discord_market_bot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscordMarketBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
